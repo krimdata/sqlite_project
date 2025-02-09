d7194f374324fb0d9a8430f90e01f13fe33e2a59
Kselami <selami-karim@hotmail.com>
2025-02-09 18:01:37 +0100

    changement de priorité

--- scripts/import_csv_to_db.py
+++ scripts/import_csv_to_db.py
@@ -2,7 +2,7 @@ import sqlite3
 import csv
 
 # Connexion à la base de données SQLite
-conn = sqlite3.connect('my_database.db')
+conn = sqlite3.connect('database/my_database.db')
 cursor = conn.cursor()
 
 # Création des tables si elles n'existent pas
